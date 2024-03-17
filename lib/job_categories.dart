3d55f1d4ccd95c378e58cf944e6282a813c81c07
Arif Hossain <alivearif120@gmail.com>
2024-03-17 18:37:20 +0100

    appBar

--- lib/job_categories.dart
+++ lib/job_categories.dart
@@ -14,13 +14,196 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 135, 0, 101),
-        title: Text(widget.title),
+        // elevation: 10,
+        // backgroundColor: const Color(0xFF765EFC),
+        // leading: const Padding(
+        //   padding: EdgeInsets.all(8.0),
+        //   child: ClipOval(
+        //     child: CircleAvatar(
+        //       child: Image(
+        //         image: AssetImage(
+        //           'assets/images/Photo_of_Arif_Hossain.jpg',
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: SizedBox(
+                height: 30,
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFFFFFFF),
+                    isDense: true,
+                    hintText: 'Search Jobs',
+                    hintStyle: TextStyle(
+                      color: Color(0xFFB3B1B1),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      size: 26,
+                      color: Colors.black,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      // horizontal: 10,
+                      vertical: 5,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.attach_money,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.email,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  height: 90,
+                  color: const Color(0xFF765EFC),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            child: const Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      '4,149',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      ' Live Jobs',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            child: const Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      '2,471',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      ' Companies',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            child: const Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      '200',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      ' New Jobs',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-     
     );
   }
 }
